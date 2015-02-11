47509d36b4b4f76d8d6afc0d91ae21de8e4c30e1
Hao SUN <hasun@zendesk.com>
2015-02-11 08:09:50 -0800

    first mock

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Hao Sun on 2/10/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Yelp/BusinessViewController.swift
@@ -0,0 +1,68 @@
+//
+//  BusinessViewController.swift
+//  Yelp
+//
+//  Created by Hao Sun on 2/10/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessViewController: UIViewController,
+                              UITableViewDelegate,
+                              UITableViewDataSource
+{
+    var client: YelpClient!
+    @IBOutlet weak var businessTableView: UITableView!
+    
+    // You can register for Yelp API keys here: http://www.yelp.com/developers/manage_api_keys
+    let yelpConsumerKey = "vxKwwcR_NMQ7WaEiQBK_CA"
+    let yelpConsumerSecret = "33QCvh5bIF5jIHR5klQr7RtBDhQ"
+    let yelpToken = "uRcRswHFYa1VkDrGV6LAW2F8clGh5JHV"
+    let yelpTokenSecret = "mqtKIxMIR4iBtBPZCmCLEb-Dz3Y"
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        businessTableView.delegate = self
+        businessTableView.dataSource = self
+        businessTableView.rowHeight = UITableViewAutomaticDimension
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+        
+        client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            println(response)
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell") as BusinessCell
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
